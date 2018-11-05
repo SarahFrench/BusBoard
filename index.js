c8d7c92604bb7fe44c84b9a70488cb48b5f14b0a
Sarah <sarah@sarahfrench.co.uk>
2018-11-05 10:50:30 +0000

    sucessfully got bus arrival data from a user input stop

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var request = require('request');
+var readline = require('readline-sync');
 
-let app_id = '84b66fad';
-let app_key = 'd5c92ab3e708aee956adf533088ad795';
+let appId = '84b66fad';
+let appKey = 'd5c92ab3e708aee956adf533088ad795';
+console.log("Please enter a bus stop number");
+let busStop = readline.prompt();
 
-request('http://www.google.com', function (error, response, body) {
+request('https://api.tfl.gov.uk/StopPoint/' + busStop + '/' + 'arrivals?app_id=' + appId + '&app_key=' + appKey, function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  a = console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the Google homepage.
 });
