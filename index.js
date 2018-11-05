2d932cdc0715420258c6c10c234334d941e9ba38
Sarah <sarah@sarahfrench.co.uk>
2018-11-05 11:16:09 +0000

    Added Logging, began printing arriving buses to console

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 var request = require('request');
 var readline = require('readline-sync');
+var log4js = require('log4js');
+var moment = require('moment');
+
+//=============================================================
+//===========================LOGGING===========================
+
+log4js.configure({
+    appenders: {
+        file: { type: 'fileSync',
+                filename: 'logs/debug.log'
+        }
+    },
+    categories: {
+        default: { appenders: ['file'],
+                   level: 'debug'
+        }
+    }
+});
+
+const logger = log4js.getLogger();
+
+
+
+//=============================================================
 
 let appId = '84b66fad';
 let appKey = 'd5c92ab3e708aee956adf533088ad795';
 console.log("Please enter a bus stop number");
 let busStop = readline.prompt();
+logger.info("User input taken")
 
 request('https://api.tfl.gov.uk/StopPoint/' + busStop + '/' + 'arrivals?app_id=' + appId + '&app_key=' + appKey, function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred
-  a = console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  if (parseInt(response.statusCode) === 404) {
+    logger.fatal("incorrect bus stop code entered by user");
+  }
+  let arrivingBuses = JSON.parse(body)
+  console.log("These are the next 5 buses at: " + arrivingBuses[0].stationName)
+  for (var i = 0; i < 5; i++) {
+    // console.log(arrivingBuses[i].timeToLive);
+
+  }
+
 });
